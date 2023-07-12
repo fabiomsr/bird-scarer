33d7963669169fb022725addbd7e0b8c3c1271ae
Fabio Santana <fabiomsr.github@gmail.com>
2023-07-12 21:35:38 +0100

    feat: load sounds from sd

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,65 @@
 #include <Arduino.h>
 #include "AudioGeneratorWAV.h"
 #include "AudioOutputI2S.h"
-#include "AudioFileSourcePROGMEM.h"
-#include <RTClib.h>
-#include <sound/day/falcon1.h>
+#include "AudioFileSourceSD.h"
+#include <esp32-hal.h>
+#include <ESP32Time.h>
 
 #define Bit_Clock_BCLK 27
 #define Word_Select_WS 26
 #define Serial_Data_SD 25
-#define GAIN 0.700
+#define GAIN 0.900
 
-RTC_DS3231 rtc;
+ESP32Time rtc(3600);
 
-AudioFileSourcePROGMEM *in;
-AudioGeneratorWAV *wav;
-AudioOutputI2S *out;
-
-const unsigned long interval = 30000;  
-
-unsigned char* daySounds[] = { falcon_1_wav };
-const int numDaySounds = sizeof(daySounds) / sizeof(daySounds[0]);
+const unsigned long interval = 5000;  
 
 void play();
-unsigned char* getRandomSound();
+int getRandomIndex();
 
 void setup() {
   Serial.begin(115200);
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); 
+  SD.begin();
+  rtc.setTime(00, 58, 19, 12, 7, 2023); 
+
   audioLogger = &Serial;
-  play();
-  
 }
 
 void loop() {
-  if (wav->isRunning()) {
-    if(!wav->loop()){
-      wav->stop();
-    }
-    return;
-  } 
-    
-  DateTime now = rtc.now();
-  int currentHour = now.hour();
+  int currentHour = rtc.getHour(true);
 
-  if (currentHour >= 6 && currentHour < 23) {
+  if (currentHour >= 6 && currentHour < 21) {
     delay(interval);
     play();
   }
 }
 
-unsigned char* getRandomSound() {
-  esp_random(); 
-  int randomIndex = esp_random() % numDaySounds; 
-  return daySounds[randomIndex];
+int getRandomIndex() {
+  return random(1, 15);
 }
 
 void play() {
-  delete wav, in, out;
-  
-  in = new AudioFileSourcePROGMEM(falcon_1_wav, sizeof(falcon_1_wav));
-  wav = new AudioGeneratorWAV();
-  out = new AudioOutputI2S();
+
+  int soundIndex = getRandomIndex();
+  String filename = "/bird"+ String(soundIndex) +".wav";
+
+  AudioFileSourceSD *in = new AudioFileSourceSD();
+  in->open(filename.c_str());
+
+  AudioGeneratorWAV *wav = new AudioGeneratorWAV();
+  AudioOutputI2S *out = new AudioOutputI2S();
+
   out->SetPinout(Bit_Clock_BCLK,Word_Select_WS,Serial_Data_SD);
   out->SetGain(GAIN);
   wav->begin(in, out);
+
+  while (wav->isRunning()) {
+    if (!wav->loop()) {
+      wav->stop();
+    }
+  }
+
+  in->close();
+
+  delete in, wav, out;
 }
